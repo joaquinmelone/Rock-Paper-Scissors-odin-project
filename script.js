4149e5874a648487fb6290a5852e194ce12ee4eb
Joaquin <joaquin.melone.zarauza@gmail.com>
2022-11-14 14:15:25 -0300

    Update script.js
    - Added the playRound function which decides the winner of a single round.

--- script.js
+++ script.js
@@ -18,4 +18,37 @@ function getComputerChoice() {
 
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    let winner
+
+    if (playerSelection === computerSelection) {
+        return (`It's a draw!`)
+    }
+
+    else if (playerSelection === "scissors" & computerSelection === "rock") {
+        return winner = true
+    }
+
+    else if (playerSelection === "rock" & computerSelection === "scissors") {
+        return winner = false
+    }
+
+    else if (playerSelection === "paper" & computerSelection === "scissors") {
+        return winner = false
+    }
+
+    else if (playerSelection === "rock" & computerSelection === "paper") {
+        return winner = true
+    }
+
+    else if (playerSelection === "paper" & computerSelection === "rock") {
+        return winner = false
+    }
+
+    else if (playerSelection === "scissors" & computerSelection === "paper") {
+        return winner = true
+    }
+}
+
 
